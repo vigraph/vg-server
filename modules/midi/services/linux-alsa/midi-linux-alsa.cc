11903c371d6c2645730a33bc91559211183e87fb
Paul Clark <paul@packetship.com>
2019-01-03 16:48:27 +0000

    Use NONBLOCK in MIDI to allow clean shutdown
    Note busy waiting is already protected by the timed poll()

--- modules/midi/services/linux-alsa/midi-linux-alsa.cc
+++ modules/midi/services/linux-alsa/midi-linux-alsa.cc
@@ -92,7 +92,7 @@ MIDIInterfaceImpl::MIDIInterfaceImpl(const Dataflow::Module *module,
   log.detail << "ALSA library version: " << SND_LIB_VERSION_STR << endl;
 
    auto status = snd_rawmidi_open(&midi, NULL, device.c_str(),
-                                  SND_RAWMIDI_SYNC);
+                                  SND_RAWMIDI_SYNC | SND_RAWMIDI_NONBLOCK);
    if (status)
    {
      log.error << "Can't open MIDI input: " << snd_strerror(status) << endl;

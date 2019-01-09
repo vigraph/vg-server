be3555bdf896f89b51e9b1d655e1046b2585444d
Paul Clark <paul@packetship.com>
2019-01-09 11:28:15 +0000

    <show-blanking> filter

---
+++ modules/laser/filters/show-blanking/show-blanking.cc
@@ -0,0 +1,69 @@
+//==========================================================================
+// ViGraph dataflow module: laser/filters/show-blanking/show-blanking.cc
+//
+// Filter to show blanked points
+//
+// Copyright (c) 2018 Paul Clark.  All rights reserved
+//==========================================================================
+
+#include "../../../vector/vector-module.h"
+#include "vg-laser.h"
+
+namespace {
+
+const auto default_blank_colour = "red";
+
+//==========================================================================
+// ShowBlanking filter
+class ShowBlankingFilter: public FrameFilter
+{
+  Colour::RGB blank_colour;
+
+  // Filter/Element virtuals
+  void accept(FramePtr frame) override;
+
+public:
+  // Construct
+  ShowBlankingFilter(const Dataflow::Module *module,
+                     const XML::Element& config);
+};
+
+//--------------------------------------------------------------------------
+// Construct from XML
+//  <show-blanking colour="red"/>
+ShowBlankingFilter::ShowBlankingFilter(const Dataflow::Module *module,
+                                       const XML::Element& config):
+  Element(module, config), FrameFilter(module, config)
+{
+  blank_colour = Colour::RGB(config.get_attr("colour", default_blank_colour));
+}
+
+//--------------------------------------------------------------------------
+// Process some data
+void ShowBlankingFilter::accept(FramePtr frame)
+{
+  for(auto& p: frame->points)
+    if (p.is_blanked()) p.c = blank_colour;
+
+  send(frame);
+}
+
+//--------------------------------------------------------------------------
+// Module definition
+Dataflow::Module module
+{
+  "show-blanking",
+  "Show Blanking",
+  "Show blanked points",
+  "laser",
+  {
+    { "colour", { {"Colour to set blanked points to", "red"},
+          Value::Type::text } }
+  },
+  { "VectorFrame" }, // inputs
+  { "VectorFrame" }  // outputs
+};
+
+} // anon
+
+VIGRAPH_ENGINE_ELEMENT_MODULE_INIT(ShowBlankingFilter, module)
---
+++ modules/laser/filters/show-blanking/test-show-blanking.cc
@@ -0,0 +1,42 @@
+//==========================================================================
+// ViGraph dataflow module: laser/filters/show-blanking/test-show-blanking.cc
+//
+// Tests for <show-blanking> filter
+//
+// Copyright (c) 2018 Paul Clark.  All rights reserved
+//==========================================================================
+
+#include "../../../vector/vector-module-test.h"
+ModuleLoader loader;
+
+TEST(ShowBlankingTest, TestBlankingIsRedAndLitPointsLeftAlone)
+{
+  const string& xml = R"(
+    <graph>
+      <svg path="M0,0 L1,0"/>
+      <show-blanking colour="red"/>
+    </graph>
+  )";
+
+  FrameGenerator gen(xml, loader);
+  Frame *frame = gen.get_frame();
+  ASSERT_FALSE(!frame);
+
+  ASSERT_EQ(2, frame->points.size());
+  EXPECT_EQ(Colour::red, frame->points[0].c);
+  EXPECT_EQ(Colour::white, frame->points[1].c);
+}
+
+int main(int argc, char **argv)
+{
+  if (argc > 1 && string(argv[1]) == "-v")
+  {
+    auto chan_out = new Log::StreamChannel{&cout};
+    Log::logger.connect(chan_out);
+  }
+
+  ::testing::InitGoogleTest(&argc, argv);
+  loader.load("../../../vector/sources/svg/vg-module-vector-source-svg.so");
+  loader.load("./vg-module-laser-filter-show-blanking.so");
+  return RUN_ALL_TESTS();
+}

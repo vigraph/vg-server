81af52a30e5b8ee325854f7d055d12565d5d9037
Paul Clark <paul@packetship.com>
2019-01-12 12:29:35 +0000

    Basic log output on enabling
    Use to confirm scene changes in CE

---
+++ modules/core/controls/log/log.cc
@@ -0,0 +1,59 @@
+//==========================================================================
+// ViGraph dataflow module: controls/log/log.cc
+//
+// Dummy control which logs a message on enabling
+//
+// Copyright (c) 2019 Paul Clark.  All rights reserved
+//==========================================================================
+
+#include "../../../module.h"
+#include <cmath>
+
+namespace {
+
+//==========================================================================
+// Log control
+class LogControl: public Control
+{
+  string message;
+
+  // Control virtuals
+  void enable();
+
+public:
+  // Construct
+  LogControl(const Module *module, const XML::Element& config);
+};
+
+//--------------------------------------------------------------------------
+// Construct from XML
+// <log>A message</log>
+LogControl::LogControl(const Module *module, const XML::Element& config):
+  Element(module, config), Control(module, config, true)  // no targets
+{
+  message = *config;
+}
+
+//--------------------------------------------------------------------------
+// Enable
+void LogControl::enable()
+{
+  Log::Summary log;
+  log << "<log>: " << message << endl;
+}
+
+//--------------------------------------------------------------------------
+// Module definition
+Dataflow::Module module
+{
+  "log",
+  "Log",
+  "Log a message on enabling",
+  "core",
+  {},
+  {}
+};
+
+} // anon
+
+VIGRAPH_ENGINE_ELEMENT_MODULE_INIT(LogControl, module)

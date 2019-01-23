b485076eb7b73bbfe4c23d0358390a539fa501d3
Alex Woods <alex@packetship.com>
2019-01-23 16:27:41 +0000

    Add tests for wav source.

--- modules/audio/sources/wav/test-wav.cc
+++ modules/audio/sources/wav/test-wav.cc
@@ -12,7 +12,7 @@
 ModuleLoader loader;
 using namespace ViGraph::Geometry;
 
-TEST(FileTest, TestNoFile)
+TEST(WavTest, TestNoFile)
 {
   const string& xml = R"(
     <graph>
@@ -20,7 +20,7 @@ TEST(FileTest, TestNoFile)
     </graph>
   )";
 
-  FragmentGenerator gen(xml, loader, 2);  // 1 to start, 1 to generate (at 1.0)
+  FragmentGenerator gen(xml, loader, 1);
   Fragment *fragment = gen.get_fragment();
   ASSERT_FALSE(!fragment);
 
@@ -28,6 +28,94 @@ TEST(FileTest, TestNoFile)
   EXPECT_EQ(88200, fragment->waveform.size());
   for(auto i=0u; i<fragment->waveform.size(); i++)
     EXPECT_EQ(0.0, fragment->waveform[i]);
+}
+
+TEST(WavTest, TestFileDefaultFormat)
+{
+  const auto data = vector<byte>{
+    0x52, 0x49, 0x46, 0x46, // "RIFF"
+    0x32, 0x00, 0x00, 0x00, // LE chunk size
+    0x57, 0x41, 0x56, 0x45, // "WAVE"
+    0x66, 0x6d, 0x74, 0x20, // "fmt "
+    0x10, 0x00, 0x00, 0x00, // LE Subchunk1 Size
+    0x03, 0x00,             // LE Audio Format (IEEE float)
+    0x02, 0x00,             // LE Channels (2)
+    0x44, 0xac, 0x00, 0x00, // LE Sample Rate (44100)
+    0x20, 0x62, 0x05, 0x00, // LE Byte Rate
+    0x08, 0x00,             // LE Block Align
+    0x20, 0x00,             // LE Bits Per Sample (32)
+    0x64, 0x61, 0x74, 0x61, // "data"
+    0x18, 0x00, 0x00, 0x00, // LE Subchunk 2 Size
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 0, 0
+    0x00, 0x00, 0x00, 0x3f, 0x00, 0x00, 0x00, 0x3f, // 0.5, 0.5
+    0x00, 0x00, 0x80, 0x3f, 0x00, 0x00, 0x80, 0x3f  // 1.0, 1.0
+  };
+  auto f = File::Path{"/tmp/test-float-44100-2.wav"};
+  f.write_all(data);
+  const string& xml = R"(
+    <graph>
+      <wav hello="hi" file="/tmp/test-float-44100-2.wav"/>
+    </graph>
+  )";
+
+  FragmentGenerator gen(xml, loader, 1);
+  Fragment *fragment = gen.get_fragment();
+  f.erase();
+  ASSERT_FALSE(!fragment);
+
+  // Should be 44100 samples for each of the 2 channels at 0
+  EXPECT_EQ(88200, fragment->waveform.size());
+
+  EXPECT_EQ(0.0, fragment->waveform[0]);
+  EXPECT_EQ(0.0, fragment->waveform[1]);
+  EXPECT_EQ(0.5, fragment->waveform[2]);
+  EXPECT_EQ(0.5, fragment->waveform[3]);
+  EXPECT_EQ(1.0, fragment->waveform[4]);
+  EXPECT_EQ(1.0, fragment->waveform[5]);
+}
+
+TEST(WavTest, TestFileFormatThatNeedsConversion)
+{
+  const auto data = vector<byte>{
+    0x52, 0x49, 0x46, 0x46, // "RIFF"
+    0x2b, 0x00, 0x00, 0x00, // LE chunk size
+    0x57, 0x41, 0x56, 0x45, // "WAVE"
+    0x66, 0x6d, 0x74, 0x20, // "fmt "
+    0x10, 0x00, 0x00, 0x00, // LE Subchunk1 Size
+    0x01, 0x00,             // LE Audio Format (PCM)
+    0x02, 0x00,             // LE Channels (2)
+    0x44, 0xac, 0x00, 0x00, // LE Sample Rate (44100)
+    0x10, 0xb1, 0x02, 0x00, // LE Byte Rate
+    0x04, 0x00,             // LE Block Align
+    0x10, 0x00,             // LE Bits Per Sample (16)
+    0x64, 0x61, 0x74, 0x61, // "data"
+    0x0c, 0x00, 0x00, 0x00, // LE Subchunk 2 Size
+    0x00, 0x00, 0x00, 0x00, // 0, 0
+    0x00, 0x40, 0x00, 0x40, // 0.5, 0.5
+    0xff, 0x7f, 0xff, 0x7f  // 1.0, 1.0
+  };
+  auto f = File::Path{"/tmp/test-pcm-44100-2.wav"};
+  f.write_all(data);
+  const string& xml = R"(
+    <graph>
+      <wav hello="hi" file="/tmp/test-pcm-44100-2.wav"/>
+    </graph>
+  )";
+
+  FragmentGenerator gen(xml, loader, 1);
+  Fragment *fragment = gen.get_fragment();
+  f.erase();
+  ASSERT_FALSE(!fragment);
+
+  // Should be 44100 samples for each of the 2 channels at 0
+  EXPECT_EQ(88200, fragment->waveform.size());
+
+  EXPECT_EQ(0.0, fragment->waveform[0]);
+  EXPECT_EQ(0.0, fragment->waveform[1]);
+  EXPECT_EQ(0.5, fragment->waveform[2]);
+  EXPECT_EQ(0.5, fragment->waveform[3]);
+  EXPECT_NEAR(1.0, fragment->waveform[4], 0.001);
+  EXPECT_NEAR(1.0, fragment->waveform[5], 0.001);
 }
 
 int main(int argc, char **argv)

edce0edf1f837fd79bbdbc3b6c1ed7dd904b230f
Alex Woods <alex@packetship.com>
2019-02-14 13:15:15 +0000

    Basic version of <spread> control.

---
+++ modules/core/controls/spread/spread.cc
@@ -0,0 +1,96 @@
+//==========================================================================
+// ViGraph dataflow module: controls/spread/spread.cc
+//
+// Animation control to spread a value to a control setting
+//
+// Copyright (c) 2018 Paul Clark.  All rights reserved
+//==========================================================================
+
+#include "../../../module.h"
+#include <cmath>
+
+namespace {
+
+//==========================================================================
+// Spread control
+class SpreadControl: public Dataflow::Control
+{
+  vector<Value> values;
+  unsigned capturing = 0;
+  unsigned pos = 0;
+  unique_ptr<Value> last_on;
+
+  // Control/Element virtuals
+  void set_property(const string& property, const SetParams& sp) override;
+
+public:
+  // Construct
+  SpreadControl(const Module *module, const XML::Element& config);
+};
+
+//--------------------------------------------------------------------------
+// Construct from XML
+//   <spread offset="0.1" property="x"/>
+SpreadControl::SpreadControl(const Module *module, const XML::Element& config):
+  Element(module, config), Control(module, config)
+{
+}
+
+//--------------------------------------------------------------------------
+// Set a control property
+void SpreadControl::set_property(const string& prop,
+                                 const SetParams& sp)
+{
+  if (prop == "trigger")
+  {
+    if (last_on)
+    {
+      send("off", *last_on);
+      last_on.reset();
+    }
+    if (capturing)
+    {
+      last_on.reset(new Value{values[pos]});
+      send("on", values[pos]);
+      if (++pos >= values.size())
+        pos = 0;
+    }
+  }
+  else if (prop == "on")
+  {
+    if (!capturing++)
+    {
+      values.clear();
+      pos = 0;
+    }
+    values.push_back(sp.v);
+  }
+  else if (prop == "off")
+  {
+    --capturing;
+  }
+}
+
+//--------------------------------------------------------------------------
+// Module definition
+Dataflow::Module module
+{
+  "spread",
+  "Spread",
+  "Spread a collection of values over time",
+  "core",
+  {
+    { "trigger", { "Note trigger", Value::Type::trigger, "@trigger", true }},
+    { "on", { "Note on", Value::Type::number, "@on", true }},
+    { "off", { "Note off", Value::Type::number, "@off", true }},
+  },
+  {
+    { "on", { "Note on", "on", Value::Type::number }},
+    { "off", { "Note off", "off", Value::Type::number }},
+  }
+
+};
+
+} // anon
+
+VIGRAPH_ENGINE_ELEMENT_MODULE_INIT(SpreadControl, module)

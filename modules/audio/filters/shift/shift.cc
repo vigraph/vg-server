230715f575d1d6476e2e0433b57506b35606b2c6
Alex Woods <alex@packetship.com>
2019-03-11 15:01:11 +0000

    Allow enabling/disabling of pitch shifter.

--- modules/audio/filters/shift/shift.cc
+++ modules/audio/filters/shift/shift.cc
@@ -21,10 +21,12 @@ class ShiftFilter: public FragmentFilter
 {
   map<Speaker, SoundTouch> sound_touch;
   double pitch = 0;
+  bool enabled = true;
 
   // Source/Element virtuals
   void set_property(const string& property, const SetParams& sp) override;
   void accept(FragmentPtr fragment) override;
+  void notify_target_of(Element *, const string& property) override;
 
 public:
   ShiftFilter(const Dataflow::Module *module, const XML::Element& config);
@@ -50,50 +52,72 @@ void ShiftFilter::set_property(const string& property, const SetParams& sp)
     for (auto &st: sound_touch)
       st.second.setPitchSemiTones(pitch);
   }
+  else if (property == "trigger")
+    enabled = true;
+  else if (property == "clear")
+  {
+    enabled = false;
+    sound_touch.clear();
+  }
 }
 
 //--------------------------------------------------------------------------
 // Process some data
 void ShiftFilter::accept(FragmentPtr fragment)
 {
-  auto max_samples = 0xffffffffffffffff;
-  for (auto& wit: fragment->waveforms)
+  if (enabled)
   {
-    const auto c = wit.first;
-    auto& w = wit.second;
-
-    auto stit = sound_touch.find(c);
-    if (stit == sound_touch.end())
-    {
-      // SoundTouch doesn't copy well, so this defers construction until
-      // it's in place
-      stit = sound_touch.emplace(piecewise_construct,
-                                 forward_as_tuple(c),
-                                 forward_as_tuple()).first;
-      stit->second.setChannels(1);
-      stit->second.setSampleRate(sample_rate);
-      stit->second.setPitchSemiTones(pitch);
-    }
-
-    stit->second.putSamples(&w[0], w.size());
-    max_samples = min(max_samples,
-                      min(w.size(), static_cast<unsigned long>(
-                                    stit->second.numSamples())));
-  }
-
-  if (max_samples)
-  {
+    auto max_samples = 0xffffffffffffffff;
     for (auto& wit: fragment->waveforms)
     {
       const auto c = wit.first;
       auto& w = wit.second;
+
       auto stit = sound_touch.find(c);
-      const auto samples = stit->second.receiveSamples(&w[0], max_samples);
-      w.resize(samples);
+      if (stit == sound_touch.end())
+      {
+        // SoundTouch doesn't copy well, so this defers construction until
+        // it's in place
+        stit = sound_touch.emplace(piecewise_construct,
+                                   forward_as_tuple(c),
+                                   forward_as_tuple()).first;
+        stit->second.setChannels(1);
+        stit->second.setSampleRate(sample_rate);
+        stit->second.setPitchSemiTones(pitch);
+      }
+
+      stit->second.putSamples(&w[0], w.size());
+      max_samples = min(max_samples,
+                        min(w.size(), static_cast<unsigned long>(
+                                      stit->second.numSamples())));
     }
 
+    if (max_samples)
+    {
+      for (auto& wit: fragment->waveforms)
+      {
+        const auto c = wit.first;
+        auto& w = wit.second;
+        auto stit = sound_touch.find(c);
+        const auto samples = stit->second.receiveSamples(&w[0], max_samples);
+        w.resize(samples);
+      }
+
+      send(fragment);
+    }
+  }
+  else
+  {
     send(fragment);
   }
+}
+
+//--------------------------------------------------------------------------
+// If recipient of triggers default to disabled
+void ShiftFilter::notify_target_of(Element *, const string& property)
+{
+  if (property == "trigger")
+    enabled = false;
 }
 
 //--------------------------------------------------------------------------
@@ -107,6 +131,8 @@ Dataflow::Module module
   {
     { "pitch", { {"Pitch semi tones (-60 to +60", "0"}, Value::Type::number,
                                                         "@pitch", true } },
+    { "trigger", { "Trigger on", Value::Type::trigger, true } },
+    { "clear", { "Clear", Value::Type::trigger, true } },
   },
   { "Audio" }, // inputs
   { "Audio" }  // outputs

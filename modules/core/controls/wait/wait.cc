98909f0df9f129943fb7c4fd419a02c4b324a27f
Alex Woods <alex@packetship.com>
2019-03-27 10:38:17 +0000

    Split set into set, trigger and wait.

---
+++ modules/core/controls/wait/wait.cc
@@ -0,0 +1,87 @@
+//==========================================================================
+// ViGraph dataflow module: controls/set/set.cc
+//
+// Control to set / alter properties on other elements
+//
+// Copyright (c) 2019 Paul Clark.  All rights reserved
+//==========================================================================
+
+#include "../../../module.h"
+#include <cmath>
+
+namespace {
+
+//==========================================================================
+// Set control
+class WaitControl: public Control
+{
+public:
+  // Configured state
+  double delay = 0.0;
+
+private:
+  Dataflow::timestamp_t delayed_until = -1.0;
+
+  // Dynamic state
+  bool triggered = false;
+
+  // Control virtuals
+  void pre_tick(const TickData& td) override;
+  void enable() override;
+
+public:
+  using Control::Control;
+
+  // Trigger
+  void set_triggered() { triggered = true; }
+};
+
+//--------------------------------------------------------------------------
+// Enable (reset)
+void WaitControl::enable()
+{
+  delayed_until = -1.0;
+}
+
+//--------------------------------------------------------------------------
+// Tick
+void WaitControl::pre_tick(const TickData& td)
+{
+  // Apply delay if necessary
+  if (triggered)
+  {
+    delayed_until = td.t + delay;
+    triggered = false;
+    return;
+  }
+
+  if (delayed_until > td.t)
+    return;
+  delayed_until = -1.0;
+
+  // We're good - do it...
+  trigger();
+}
+
+//--------------------------------------------------------------------------
+// Module definition
+Dataflow::Module module
+{
+  "wait",
+  "Wait",
+  "Wait another Element",
+  "core",
+  {
+    { "for", { "Time to wait for", Value::Type::number,
+               static_cast<double Element::*>(&WaitControl::delay),
+               true } },
+    { "trigger", { "Trigger", Value::Type::trigger,
+             static_cast<void (Element::*)()>(&WaitControl::set_triggered),
+                   true } }
+  },
+  { { "trigger", { "Trigger", "trigger", Value::Type::trigger }}}
+};
+
+} // anon
+
+VIGRAPH_ENGINE_ELEMENT_MODULE_INIT(WaitControl, module)

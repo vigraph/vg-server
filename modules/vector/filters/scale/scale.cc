82df60e364965e1d0b3da45238c52c4e00ac36c6
Alex Woods <alex@packetship.com>
2019-03-28 14:53:20 +0000

    Use new structure for ScaleFilter.

--- modules/vector/filters/scale/scale.cc
+++ modules/vector/filters/scale/scale.cc
@@ -15,48 +15,21 @@ namespace {
 // Scale filter
 class ScaleFilter: public FrameFilter
 {
+public:
   double sx{0.0};
   double sy{0.0};
   double sz{0.0};
 
+private:
   // Filter/Element virtuals
-  void set_property(const string& property, const SetParams& sp) override;
   void accept(FramePtr frame) override;
 
 public:
-  // Construct
-  ScaleFilter(const Dataflow::Module *module, const XML::Element& config);
+  using FrameFilter::FrameFilter;
+
+  // Getters/Setters
+  void set_all(double s) { sx = s; sy = s; sz = s; }
 };
-
-//--------------------------------------------------------------------------
-// Construct from XML
-//  <scale all="0.5"/>
-//  <scale x="0.5" y="1.0" z="2.0"/>
-ScaleFilter::ScaleFilter(const Dataflow::Module *module,
-                         const XML::Element& config):
-  FrameFilter(module, config)
-{
-  if (config.has_attr("all"))
-    sx = sy = sz = config.get_attr_real("all", 1.0);
-  else
-  {
-    sx = config.get_attr_real("x", 1.0);
-    sy = config.get_attr_real("y", 1.0);
-    sz = config.get_attr_real("z", 1.0);
-  }
-}
-
-//--------------------------------------------------------------------------
-// Set a control property
-void ScaleFilter::set_property(const string& property, const SetParams& sp)
-{
-  if (property == "all")
-  { update_prop(sx, sp); update_prop(sy, sp); update_prop(sz, sp); }
-  else if (property == "x") update_prop(sx, sp);
-  else if (property == "y") update_prop(sy, sp);
-  else if (property == "z") update_prop(sz, sp);
-
-}
 
 //--------------------------------------------------------------------------
 // Process some data
@@ -83,10 +56,13 @@ Dataflow::Module module
   "vector",
   {
     { "all", { {"Factor to scale all axes","1.0"}, Value::Type::number,
-                                                     "all", true } },
-    { "x", { {"Factor to scale X","1.0"}, Value::Type::number, "x", true } },
-    { "y", { {"Factor to scale Y","1.0"}, Value::Type::number, "y", true } },
-    { "z", { {"Factor to scale Z","1.0"}, Value::Type::number, "z", true } }
+               { &ScaleFilter::set_all }, true } },
+    { "x", { {"Factor to scale X","1.0"}, Value::Type::number,
+             &ScaleFilter::sx, true } },
+    { "y", { {"Factor to scale Y","1.0"}, Value::Type::number,
+             &ScaleFilter::sy, true } },
+    { "z", { {"Factor to scale Z","1.0"}, Value::Type::number,
+             &ScaleFilter::sz, true } },
   },
   { "VectorFrame" }, // inputs
   { "VectorFrame" }  // outputs

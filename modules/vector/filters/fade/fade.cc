7cb8d80f3a4d5cc3f4a44a572dfd32d7e657b16c
Alex Woods <alex@packetship.com>
2019-03-28 11:15:03 +0000

    Use new structure for FadeFilter.

--- modules/vector/filters/fade/fade.cc
+++ modules/vector/filters/fade/fade.cc
@@ -15,47 +15,21 @@ namespace {
 // Fade filter
 class FadeFilter: public FrameFilter
 {
-  double fr{0.0};
-  double fg{0.0};
-  double fb{0.0};
+public:
+  double fr{1.0};
+  double fg{1.0};
+  double fb{1.0};
 
+private:
   // Filter/Element virtuals
-  void set_property(const string& property, const SetParams& sp) override;
   void accept(FramePtr frame) override;
 
 public:
-  // Construct
-  FadeFilter(const Dataflow::Module *module, const XML::Element& config);
+  using FrameFilter::FrameFilter;
+
+  // Getters/Setters
+  void set_all(double f) { fr = f; fg = f; fb = f; }
 };
-
-//--------------------------------------------------------------------------
-// Construct from XML
-//  <fade all="0.5"/>
-//  <fade r="0.9" g="1.0" b="1.1"/>
-FadeFilter::FadeFilter(const Dataflow::Module *module,
-                                 const XML::Element& config):
-  FrameFilter(module, config)
-{
-  if (config.has_attr("all"))
-    fr = fg = fb = config.get_attr_real("all", 1.0);
-  else
-  {
-    fr = config.get_attr_real("r", 1.0);
-    fg = config.get_attr_real("g", 1.0);
-    fb = config.get_attr_real("b", 1.0);
-  }
-}
-
-//--------------------------------------------------------------------------
-// Set a control property
-void FadeFilter::set_property(const string& property, const SetParams& sp)
-{
-  if (property == "all")
-  { update_prop(fr, sp); update_prop(fg, sp); update_prop(fb, sp); }
-  else if (property == "r") update_prop(fr, sp);
-  else if (property == "g") update_prop(fg, sp);
-  else if (property == "b") update_prop(fb, sp);
-}
 
 //--------------------------------------------------------------------------
 // Process some data
@@ -89,11 +63,16 @@ Dataflow::Module module
   "Fade all points to a multiple of their brightness",
   "vector",
   {
-    { "all", { {"Factor to fade all channels","1.0"}, Value::Type::number,
-                                                     "all", true } },
-    { "r", { {"Factor to fade red","1.0"}, Value::Type::number, "r", true } },
-    { "g", { {"Factor to fade green","1.0"}, Value::Type::number, "g", true } },
-    { "b", { {"Factor to fade blue","1.0"}, Value::Type::number, "b", true } }
+    { "all", { "Factor to fade all channels", Value::Type::number,
+               { nullptr,
+                 static_cast<void (Element::*)(double)>(&FadeFilter::set_all) },
+               true, true } },
+    { "r", { "Factor to fade red", Value::Type::number,
+             static_cast<double Element::*>(&FadeFilter::fr), true } },
+    { "g", { "Factor to fade green", Value::Type::number,
+             static_cast<double Element::*>(&FadeFilter::fg), true } },
+    { "b", { "Factor to fade blue", Value::Type::number,
+             static_cast<double Element::*>(&FadeFilter::fb), true } },
   },
   { "VectorFrame" }, // inputs
   { "VectorFrame" }  // outputs

e7bedc67e20acd26b03f7291af7b9c5101950a85
Alex Woods <alex@packetship.com>
2019-04-01 10:15:52 +0100

    Use new structure for RoundControl.

--- modules/core/controls/round/round.cc
+++ modules/core/controls/round/round.cc
@@ -14,65 +14,28 @@ namespace {
 // Round control
 class RoundControl: public Dataflow::Control
 {
-  string property;
-  double n = 1.0; // Enumerator
-  double d = 1.0; // Divisor
-
+private:
   // Control/Element virtuals
-  void set_property(const string& property, const SetParams& sp) override;
-  Dataflow::Value::Type get_property_type(const string& property) override;
+  void update() override;
 
 public:
+  double n = 1.0; // Enumerator
+  double d = 1.0; // Divisor
+  double value = 0.0;
+
   // Construct
-  RoundControl(const Module *module, const XML::Element& config);
+  using Control::Control;
 };
 
 //--------------------------------------------------------------------------
-// Construct from XML
-//   <round property="x" n="1" d="2" />
-RoundControl::RoundControl(const Module *module, const XML::Element& config):
-  Control(module, config)
+// Update
+void RoundControl::update()
 {
-  property = config["property"];
-  n = config.get_attr_int("n", n);
-  d = config.get_attr_int("d", d);
-}
-
-//--------------------------------------------------------------------------
-// Set a control property
-void RoundControl::set_property(const string& prop, const SetParams& sp)
-{
-  if (prop == property)
-  {
-    if (!n || !d)
-      return;
-    // Round the value and pass on
-    const auto value = n * round(d * sp.v.d / n) / d;
-    SetParams nsp(Dataflow::Value{value});
-    send(nsp);
-  }
-  else if (prop == "n")
-  {
-    update_prop(n, sp);
-  }
-  else if (prop == "d")
-  {
-    update_prop(d, sp);
-  }
-}
-
-//--------------------------------------------------------------------------
-// Get control property types
-Dataflow::Value::Type RoundControl::get_property_type(const string& prop)
-{
-  if (prop == property)
-    return Dataflow::Value::Type::number;
-  else if (prop == "n")
-    return Dataflow::Value::Type::number;
-  else if (prop == "d")
-    return Dataflow::Value::Type::number;
-  else
-    return Dataflow::Value::Type::invalid;
+  if (!n || !d)
+    return;
+  // Round the value and pass on
+  const auto v = n * round(d * value / n) / d;
+  send(Dataflow::Value{v});
 }
 
 //--------------------------------------------------------------------------
@@ -84,11 +47,12 @@ Dataflow::Module module
   "Round a control value",
   "core",
   {
-    { "property", { "Property to set", Value::Type::text } },
-    { "n", { {"Enumerator", ""}, Value::Type::number, "@n", true } },
-    { "d", { {"Divisor", ""}, Value::Type::number, "@d", true } },
+    { "value", { "Value to round", Value::Type::number,
+                 &RoundControl::value, true } },
+    { "n", { "Enumerator", Value::Type::number, &RoundControl::n , true } },
+    { "d", { "Divisor", Value::Type::number, &RoundControl::d, true } },
   },
-  { { "", { "Value output", "", Value::Type::number }}}
+  { { "value", { "Value output", "value", Value::Type::number }}}
 };
 
 } // anon

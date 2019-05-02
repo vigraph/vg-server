497049615d05730b8172d0097a30218ac4949b3c
Alex Woods <alex@packetship.com>
2019-05-02 16:03:48 +0100

    Look for config files in same dir as exe for windows.

--- engine/main.cc
+++ engine/main.cc
@@ -11,10 +11,12 @@
 namespace {
 const auto server_name    = "ViGraph dataflow engine daemon";
 const auto server_version = VERSION;
+const auto default_config_file_name = "engine.cfg.xml";
+const auto default_licence_file_name = "licence.xml";
 #ifdef DEBUG
-const auto default_config_file = "engine.cfg.xml";
+const auto default_config_file = default_config_file_name;
 #else
-const auto default_config_file = "/etc/vigraph/engine.cfg.xml";
+const auto default_config_file = "/etc/vigraph/" + default_config_file_name;
 #endif
 const auto config_file_root = "engine";
 const auto default_log_file = "/var/log/vigraph/engine.log";
@@ -29,9 +31,22 @@ using namespace ViGraph::Engine;
 // Main
 int main(int argc, char **argv)
 {
+#if defined(PLATFORM_WINDOWS)
+  winsock_initialise();
+  wchar_t p[MAX_PATH];
+  GetModuleFileNameW(nullptr, p, MAX_PATH);
+  const auto path = File::Path(Text::UTF8::encode(&p[0]));
+  const auto licence_file = path.dirname() + "\\" + default_licence_file_name;
+  const auto config_file = path.dirname() + "\\" + default_config_file_name;
+  Server server(licence_file);
+  Daemon::Shell shell(server, server_name, server_version,
+                      config_file, config_file_root,
+                      default_log_file, pid_file);
+#else
   Server server;
   Daemon::Shell shell(server, server_name, server_version,
                       default_config_file, config_file_root,
                       default_log_file, pid_file);
+#endif
   return shell.start(argc, argv);
 }

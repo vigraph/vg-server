c0adccc50165aa0cbc7b7d475d0156ec99592287
Alex Woods <alex@packetship.com>
2019-06-06 12:48:17 +0100

    Open a webview window on Windows.

--- engine/main.cc
+++ engine/main.cc
@@ -6,6 +6,12 @@
 // Copyright (c) 2017 Paul Clark.  All rights reserved
 //==========================================================================
 
+#if defined(PLATFORM_WINDOWS)
+#define WEBVIEW_WINAPI
+#define WEBVIEW_IMPLEMENTATION
+#include <webview.h>
+#endif
+
 #include "engine.h"
 
 namespace {
@@ -13,6 +19,8 @@ const auto server_name    = "ViGraph dataflow engine daemon";
 const auto server_version = VERSION;
 
 #if defined(PLATFORM_WINDOWS)
+const auto application_name = "ViGraph Create Pro";
+const auto application_url = "http://localhost:33380/";
 const auto default_licence_file_name = "licence.xml";
 const auto default_config_file_name = "engine.cfg.xml";
 #else
@@ -35,9 +43,9 @@ using namespace ViGraph::Engine;
 
 //--------------------------------------------------------------------------
 // Main
-int main(int argc, char **argv)
-{
 #if defined(PLATFORM_WINDOWS)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
   winsock_initialise();
   wchar_t p[MAX_PATH];
   GetModuleFileNameW(nullptr, p, MAX_PATH);
@@ -45,14 +53,22 @@ int main(int argc, char **argv)
   const auto licence_file = path.dirname() + "\\" + default_licence_file_name;
   const auto config_file = path.dirname() + "\\" + default_config_file_name;
   Server server(licence_file);
-  Daemon::Shell shell(server, server_name, server_version,
-                      config_file, config_file_root,
-                      default_log_file, pid_file);
+  auto on_run = []()
+    {
+      webview(application_name, application_url, 800, 600, 1);
+    };
+  Daemon::WindowsShell shell(server, server_name, server_version, on_run,
+                             config_file, config_file_root,
+                             default_log_file, pid_file);
+  return shell.start(__argc, __argv);
+}
 #else
+int main(int argc, char **argv)
+{
   Server server(default_licence);
   Daemon::Shell shell(server, server_name, server_version,
                       default_config_file, config_file_root,
                       default_log_file, pid_file);
-#endif
   return shell.start(argc, argv);
 }
+#endif

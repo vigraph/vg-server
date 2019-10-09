92cade8a82cf111635f8297592b4354b099a989a
Paul Clark <paul@packetship.com>
2019-10-09 18:14:52 +0100

    Simple vg-compile utility
    bug:comment:1530

---
+++ utils/compile-vg/main.cc
@@ -0,0 +1,83 @@
+//==========================================================================
+// ViGraph vector graphics: main.cc
+//
+// Utility to compile 'vg' text format to JSON graph
+//
+// Copyright (c) 2019 Paul Clark.  All rights reserved
+//==========================================================================
+
+#include "vg-compiler.h"
+#include <fstream>
+
+using namespace std;
+using namespace ObTools;
+using namespace ViGraph;
+
+//--------------------------------------------------------------------------
+// Main
+int main(int argc, char **argv)
+{
+  if (argc < 2)
+  {
+    cout << "ViGraph 'vg' compiler v0.1\n\n";
+    cout << "Usage:\n";
+    cout << "  " << argv[0] << " <input file> <output file>\n";
+    cout << endl;
+    cout << "Both <input file> and <output file> can be '-' for pipeline\n";
+    return 2;
+  }
+
+  JSON::Value root;
+
+  try
+  {
+    const string vgf(argv[argc-2]);
+    if (vgf == "-")
+    {
+      Compiler::Parser parser(cin);
+      root = parser.get_json();
+    }
+    else
+    {
+      ifstream in(vgf);
+      if (!in)
+      {
+        cerr << "Can't read 'vg' file " << vgf << endl;
+        return 2;
+      }
+
+      Compiler::Parser parser(in);
+      root = parser.get_json();
+    }
+  }
+  catch (Compiler::Exception e)
+  {
+    cerr << "Compile failed:\n" << e.error << endl;
+    return 4;
+  }
+
+  // Write out
+  const string jsf(argv[argc-1]);
+  if (jsf == "-")
+  {
+    cout << root;
+  }
+  else
+  {
+    cout << "Writing JSON file " << jsf << endl;
+    ofstream out(jsf);
+    if (!out)
+    {
+      cerr << "Can't write file " << jsf << endl;
+      return 2;
+    }
+
+    out << root;
+  }
+
+  return 0;
+}
+
+
+
+

af5139d0a0bc8c12a4f7645f5fe1bd286e7dc056
Paul Clark <paul@packetship.com>
2019-10-31 13:51:17 +0000

    Basic Artnet output and bitmap rendering
    Can only address 1 Universe at the moment

---
+++ modules/dmx/artnet-out/artnet-out.cc
@@ -0,0 +1,143 @@
+//==========================================================================
+// ViGraph dataflow module: dmx/artnet-out/artnet-out.cc
+//
+// Artnet (DMX over UDP) output
+//
+// Copyright (c) 2019 Paul Clark.  All rights reserved
+//==========================================================================
+
+#include "../dmx-module.h"
+#include "vg-artnet.h"
+
+namespace {
+
+using namespace ViGraph::Dataflow;
+const auto default_source_address = "0.0.0.0";
+const auto default_frame_rate = 25;
+const auto default_universe{0};
+
+//==========================================================================
+// ArtNetOut filter
+class ArtNetOut: public SimpleElement
+{
+private:
+  Net::EndPoint source, destination;
+
+  // State
+  unique_ptr<Net::UDPSocket> socket;
+  uint8_t sequence{0};
+
+  // Element virtuals
+  void setup() override;
+  void tick(const TickData& td) override;
+
+  // Clone
+  ArtNetOut *create_clone() const override
+  {
+    return new ArtNetOut{module};
+  }
+
+  void shutdown() override;
+
+public:
+  using SimpleElement::SimpleElement;
+
+  // Settings
+  Setting<string> host_address;
+  Setting<int> host_port{ArtNet::udp_port};
+  Setting<string> source_address{default_source_address};
+  Setting<double> frame_rate{default_frame_rate};
+  Setting<double> universe{default_universe};
+
+  // Input
+  Input<DMXState> input;
+};
+
+//--------------------------------------------------------------------------
+// Setup after config
+void ArtNetOut::setup()
+{
+  Log::Streams log;
+
+  destination = Net::EndPoint(Net::IPAddress(host_address), host_port);
+  log.summary << "Creating ArtNet transmitter to " << destination << endl;
+
+  source = Net::EndPoint(Net::IPAddress(source_address), 0);
+  // Bind to local port
+  socket.reset(new Net::UDPSocket(source, true));
+
+  source = socket->local();
+  log.detail << "ArtNet transmitter bound to local address " << source << endl;
+
+  input.set_sample_rate(frame_rate);
+}
+
+//--------------------------------------------------------------------------
+// Tick data
+void ArtNetOut::tick(const TickData& td)
+{
+  if (!socket) return;
+
+  const auto nsamples = td.samples_in_tick(frame_rate);
+  sample_iterate(nsamples, {}, tie(input), {},
+                 [&](const DMXState& input)
+  {
+    if (!sequence) sequence++;  // Avoid 0
+    ArtNet::DMXPacket dmx_packet(sequence++, universe);
+    dmx_packet.data = input.channels;
+
+    auto size = dmx_packet.length();
+
+    // Create a packet
+    vector<unsigned char> packet;
+    packet.resize(size);
+    Channel::BlockWriter bw(packet.data(), size);
+    dmx_packet.write(bw);
+
+    // Send it
+    try
+    {
+      socket->sendto(packet.data(), size, 0, destination);
+    }
+    catch (Net::SocketError e)
+    {
+      Log::Error log;
+      log << "ArtNet transmit socket error: " << e.get_string() << endl;
+      return;
+    }
+  });
+}
+
+//--------------------------------------------------------------------------
+// Shut down
+void ArtNetOut::shutdown()
+{
+  Log::Detail log;
+  log << "Shutting down ArtNet transmit server\n";
+  socket.reset();
+}
+
+//--------------------------------------------------------------------------
+// Module definition
+Dataflow::SimpleModule module
+{
+  "artnet-out",
+  "ArtNet Output",
+  "dmx",
+  {
+    { "address",         &ArtNetOut::host_address      },
+    { "port",            &ArtNetOut::host_port         },
+    { "source=address",  &ArtNetOut::source_address    },
+    { "frame-rate",      &ArtNetOut::frame_rate        },
+    { "universe",        &ArtNetOut::universe          }
+  },
+  {
+    { "input",           &ArtNetOut::input }
+  },
+  {}
+};
+
+} // anon
+
+VIGRAPH_ENGINE_ELEMENT_MODULE_INIT(ArtNetOut, module)
+

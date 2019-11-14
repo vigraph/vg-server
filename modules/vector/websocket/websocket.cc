3c4bd7360f004a26a623f5fe8c3e446c891607db
Alex Woods <alex@packetship.com>
2019-11-14 15:21:19 +0000

    Make sure websocket gets its number of samples to produce from td.

--- modules/vector/websocket/websocket.cc
+++ modules/vector/websocket/websocket.cc
@@ -161,9 +161,10 @@ void WebSocket::setup()
 
 //--------------------------------------------------------------------------
 // Tick data
-void WebSocket::tick(const TickData&)
+void WebSocket::tick(const TickData& td)
 {
-  const auto nsamples = frame_rate;
+  const auto sample_rate = input.get_sample_rate();
+  const auto nsamples = td.samples_in_tick(sample_rate);
   sample_iterate(nsamples, {}, tie(input), {},
                  [&](const Frame& input)
   {

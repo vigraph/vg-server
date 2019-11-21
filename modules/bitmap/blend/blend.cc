0cf38179c69a58e941dc8b152822d7dcb8133ff7
Paul Clark <paul@packetship.com>
2019-11-21 10:25:12 +0000

    Switchable bitmap blend types

--- modules/bitmap/blend/blend.cc
+++ modules/bitmap/blend/blend.cc
@@ -9,16 +9,26 @@
 
 #include "../bitmap-module.h"
 #include "../../colour/colour-module.h"
+#include "blend-type.h"
 
 namespace {
 
 //==========================================================================
 // Blend
-class Blend: public SimpleElement
+class Blend: public DynamicElement
 {
 private:
-  // /Element virtuals
+  BlendType last_type{BlendType::none};
+
+  // Internal
+  void tick_horizontal(const TickData& td);
+  void tick_vertical(const TickData& td);
+  void tick_rectangular(const TickData& td);
+  void tick_radial(const TickData& td);
+
+  // Element virtuals
   void tick(const TickData& td) override;
+  void setup(const SetupContext& context) override;
 
   // Clone
   Blend *create_clone() const override
@@ -27,23 +37,163 @@ private:
   }
 
 public:
-  using SimpleElement::SimpleElement;
+  using DynamicElement::DynamicElement;
+
+  // Settings
+  Setting<BlendType> type{BlendType::none};
 
   // Inputs
   Input<double> width{1};
   Input<double> height{1};
-  Input<Colour::RGB> top_left;
-  Input<Colour::RGB> top_right;
-  Input<Colour::RGB> bottom_left;
-  Input<Colour::RGB> bottom_right;
+
+  // Horizontal
+  Input<Colour::RGB> left{Colour::black};
+  Input<Colour::RGB> right{Colour::black};
+
+  // Vertical
+  Input<Colour::RGB> top{Colour::black};
+  Input<Colour::RGB> bottom{Colour::black};
+
+  // Rectangular
+  Input<Colour::RGB> top_left{Colour::black};
+  Input<Colour::RGB> top_right{Colour::black};
+  Input<Colour::RGB> bottom_left{Colour::black};
+  Input<Colour::RGB> bottom_right{Colour::black};
+
+  // Radial
+  Input<Colour::RGB> centre{Colour::black};
+  Input<Colour::RGB> edge{Colour::black};
+  Input<double> radius{0.5};
 
   // Output
   Output<Bitmap::Group> output;
 };
 
 //--------------------------------------------------------------------------
-// Generate a bitmap
-void Blend::tick(const TickData& td)
+// Setup inputs
+void Blend::setup(const SetupContext&)
+{
+  // Do nothing if same type
+  if (type == last_type) return;
+
+  // Get rid of old inputs
+  switch (last_type)
+  {
+    case BlendType::none: break;
+
+    case BlendType::horizontal:
+      module.erase_input("left");
+      module.erase_input("right");
+      break;
+
+    case BlendType::vertical:
+      module.erase_input("top");
+      module.erase_input("bottom");
+      break;
+
+    case BlendType::rectangular:
+      module.erase_input("top-left");
+      module.erase_input("top-right");
+      module.erase_input("bottom-left");
+      module.erase_input("bottom-right");
+      break;
+
+    case BlendType::radial:
+      module.erase_input("centre");
+      module.erase_input("edge");
+      module.erase_input("radius");
+      break;
+  }
+
+  // Add new inputs
+  switch (type)
+  {
+    case BlendType::none: break;
+
+    case BlendType::horizontal:
+      module.add_input("left", &Blend::left);
+      module.add_input("right", &Blend::right);
+      break;
+
+    case BlendType::vertical:
+      module.add_input("top", &Blend::top);
+      module.add_input("bottom", &Blend::bottom);
+      break;
+
+    case BlendType::rectangular:
+      module.add_input("top-left", &Blend::top_left);
+      module.add_input("top-right", &Blend::top_right);
+      module.add_input("bottom-left", &Blend::bottom_left);
+      module.add_input("bottom-right", &Blend::bottom_right);
+      break;
+
+    case BlendType::radial:
+      module.add_input("centre", &Blend::centre);
+      module.add_input("edge",   &Blend::edge);
+      module.add_input("radius", &Blend::radius);
+      break;
+  }
+
+  last_type = type;
+}
+
+//--------------------------------------------------------------------------
+// Horizontal blend
+void Blend::tick_horizontal(const TickData& td)
+{
+  const auto nsamples = td.samples_in_tick(output.get_sample_rate());
+  sample_iterate(nsamples, {}, tie(width, height, left, right),
+                 tie(output),
+                 [&](double width, double height,
+                     const Colour::RGB& left,
+                     const Colour::RGB& right,
+                     Bitmap::Group& output)
+  {
+    Bitmap::Rectangle rect(width, height);
+
+    // Blend horizontally, repeated for all columns
+    for(auto x=0; x<width; x++)
+    {
+      auto x_frac = (width>1) ? x/(width-1) : 0;
+      auto c = left.blend_with(right, x_frac);
+      for(auto y=0; y<height; y++)
+        rect.set(x, y, c);
+    }
+
+    output.add(rect);
+  });
+}
+
+//--------------------------------------------------------------------------
+// Vertical blend
+void Blend::tick_vertical(const TickData& td)
+{
+  const auto nsamples = td.samples_in_tick(output.get_sample_rate());
+  sample_iterate(nsamples, {}, tie(width, height, top, bottom),
+                 tie(output),
+                 [&](double width, double height,
+                     const Colour::RGB& top,
+                     const Colour::RGB& bottom,
+                     Bitmap::Group& output)
+  {
+    Bitmap::Rectangle rect(width, height);
+
+    // Blend vertically, repeated for all rows
+    for(auto y=0; y<height; y++)
+    {
+      auto y_frac = (height>1) ? y/(height-1) : 0;
+      auto c = top.blend_with(bottom, y_frac);
+      for(auto x=0; x<width; x++)
+        rect.set(x, y, c);
+    }
+
+    output.add(rect);
+  });
+}
+
+//--------------------------------------------------------------------------
+// Rectangular blend
+void Blend::tick_rectangular(const TickData& td)
 {
   const auto nsamples = td.samples_in_tick(output.get_sample_rate());
   sample_iterate(nsamples, {}, tie(width, height, top_left, top_right,
@@ -77,20 +227,76 @@ void Blend::tick(const TickData& td)
 }
 
 //--------------------------------------------------------------------------
+// Radial blend
+void Blend::tick_radial(const TickData& td)
+{
+  const auto nsamples = td.samples_in_tick(output.get_sample_rate());
+  sample_iterate(nsamples, {}, tie(width, height, centre, edge, radius),
+                 tie(output),
+                 [&](double width, double height,
+                     const Colour::RGB& centre,
+                     const Colour::RGB& edge,
+                     double radius,
+                     Bitmap::Group& output)
+  {
+    Bitmap::Rectangle rect(width, height);
+
+    for(auto y=0; y<height; y++)
+    {
+      for(auto x=0; x<width; x++)
+      {
+        Point p(x, y);
+        Point o(width/2, height/2);
+        auto d = p.distance_to(o);
+        auto f = min(1.0, (radius>0)?(d/radius/width):1.0);
+        auto c = centre.blend_with(edge, f);
+        rect.set(x, y, c);
+      }
+    }
+
+    output.add(rect);
+  });
+}
+
+//--------------------------------------------------------------------------
+// Generate a bitmap
+void Blend::tick(const TickData& td)
+{
+  switch (type)
+  {
+    case BlendType::none: break;
+
+    case BlendType::horizontal:
+      tick_horizontal(td);
+      break;
+
+    case BlendType::vertical:
+      tick_vertical(td);
+      break;
+
+    case BlendType::rectangular:
+      tick_rectangular(td);
+      break;
+
+    case BlendType::radial:
+      tick_radial(td);
+      break;
+  }
+}
+
+//--------------------------------------------------------------------------
 // Module definition
-Dataflow::SimpleModule module
+Dataflow::DynamicModule module
 {
   "blend",
   "Bitmap blend",
   "bitmap",
-  {},
+  {
+    { "type", &Blend::type }
+  },
   {
     { "width",        &Blend::width        },
-    { "height",       &Blend::height       },
-    { "top-left",     &Blend::top_left     },
-    { "top-right",    &Blend::top_right    },
-    { "bottom-left",  &Blend::bottom_left  },
-    { "bottom-right", &Blend::bottom_right }
+    { "height",       &Blend::height       }
   },
   {
     { "output",    &Blend::output    }
--- modules/bitmap/blend/test-blend.cc
+++ modules/bitmap/blend/test-blend.cc
@@ -9,6 +9,7 @@
 #include "../bitmap-module.h"
 #include "../../colour/colour-module.h"
 #include "../../module-test.h"
+#include "blend-type.h"
 
 class BlendTest: public GraphTester
 {
@@ -21,11 +22,113 @@ public:
 
 const auto sample_rate = 1;
 
-TEST_F(BlendTest, Test2DBlend4Corners)
+TEST_F(BlendTest, TestHorizontal)
 {
   auto& blend = add("bitmap/blend")
+                .set("type", BlendType::horizontal)
                 .set("width", 5.0)
                 .set("height", 3.0);
+  setup(blend);
+
+  // left
+  auto cl_data = vector<Colour::RGB>(1);
+  cl_data[0] = Colour::black;
+  auto& cls = add_source(cl_data);
+  cls.connect("output", blend, "left");
+
+  // right
+  auto cr_data = vector<Colour::RGB>(1);
+  cr_data[0] = Colour::red;
+  auto& crs = add_source(cr_data);
+  crs.connect("output", blend, "right");
+
+  // sink
+  auto bitmaps = vector<Bitmap::Group>{};
+  auto& sink = add_sink(bitmaps, sample_rate);
+  blend.connect("output", sink, "input");
+
+  run();
+
+  ASSERT_EQ(sample_rate, bitmaps.size());
+  const auto& bitmap = bitmaps[0];
+  ASSERT_EQ(1, bitmap.items.size());
+
+  const auto& b0 = bitmap.items[0];
+  EXPECT_EQ(0, b0.pos.x);
+  EXPECT_EQ(0, b0.pos.y);
+  EXPECT_EQ(0, b0.pos.z);
+  ASSERT_EQ(5, b0.rect.get_width());
+  ASSERT_EQ(3, b0.rect.get_height());
+
+  for(auto i=0u; i<5; i++)
+  {
+    for(auto j=0u; j<3; j++)
+    {
+      auto c = b0.rect(i, j);
+      EXPECT_NEAR(i/4.0, c.r, 1e-8) << i << "," << j;
+      EXPECT_EQ(0, c.g) << i << "," << j;
+      EXPECT_EQ(0, c.b) << i << "," << j;
+    }
+  }
+}
+
+TEST_F(BlendTest, TestVertical)
+{
+  auto& blend = add("bitmap/blend")
+                .set("type", BlendType::vertical)
+                .set("width", 5.0)
+                .set("height", 3.0);
+  setup(blend);
+
+  // top
+  auto ct_data = vector<Colour::RGB>(1);
+  ct_data[0] = Colour::black;
+  auto& cts = add_source(ct_data);
+  cts.connect("output", blend, "top");
+
+  // bottom
+  auto cb_data = vector<Colour::RGB>(1);
+  cb_data[0] = Colour::red;
+  auto& cbs = add_source(cb_data);
+  cbs.connect("output", blend, "bottom");
+
+  // sink
+  auto bitmaps = vector<Bitmap::Group>{};
+  auto& sink = add_sink(bitmaps, sample_rate);
+  blend.connect("output", sink, "input");
+
+  run();
+
+  ASSERT_EQ(sample_rate, bitmaps.size());
+  const auto& bitmap = bitmaps[0];
+  ASSERT_EQ(1, bitmap.items.size());
+
+  const auto& b0 = bitmap.items[0];
+  EXPECT_EQ(0, b0.pos.x);
+  EXPECT_EQ(0, b0.pos.y);
+  EXPECT_EQ(0, b0.pos.z);
+  ASSERT_EQ(5, b0.rect.get_width());
+  ASSERT_EQ(3, b0.rect.get_height());
+
+  for(auto i=0u; i<5; i++)
+  {
+    for(auto j=0u; j<3; j++)
+    {
+      auto c = b0.rect(i, j);
+      EXPECT_NEAR(j/2.0, c.r, 1e-8) << i << "," << j;
+      EXPECT_EQ(0, c.g) << i << "," << j;
+      EXPECT_EQ(0, c.b) << i << "," << j;
+    }
+  }
+}
+
+TEST_F(BlendTest, TestRectangular)
+{
+  auto& blend = add("bitmap/blend")
+                .set("type", BlendType::rectangular)
+                .set("width", 5.0)
+                .set("height", 3.0);
+  setup(blend);
 
   // top-left
   auto ctl_data = vector<Colour::RGB>(1);

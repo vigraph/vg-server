81cf0f94c7a8fd1ea89843f8dee1fea4cb01a935
Alex Woods <alex@packetship.com>
2020-01-13 14:05:26 +0000

    Make MIDI key in accept range of keys.

--- modules/midi/key-in/key-in.cc
+++ modules/midi/key-in/key-in.cc
@@ -24,14 +24,14 @@ private:
     return new KeyIn{module};
   }
 
-  Number last_velocity{0.0};
-
 public:
   using SimpleElement::SimpleElement;
 
   Input<Number> channel{-1};
-  Input<Number> key{-1};
+  Input<Number> min{-1};
+  Input<Number> max{-1};
   Input<MIDI::Event> input;
+  Output<Number> key;
   Output<Number> velocity;
   Output<Trigger> note_on;
   Output<Trigger> note_off;
@@ -41,24 +41,29 @@ public:
 // Generate a fragment
 void KeyIn::tick(const TickData& td)
 {
-  const auto sample_rate = max(velocity.get_sample_rate(),
-                               max(note_on.get_sample_rate(),
-                                   note_off.get_sample_rate()));
+  const auto sample_rate = std::max(key.get_sample_rate(),
+                                    std::max(velocity.get_sample_rate(),
+                                      std::max(note_on.get_sample_rate(),
+                                               note_off.get_sample_rate())));
   const auto nsamples = td.samples_in_tick(sample_rate);
-  sample_iterate(td, nsamples, {}, tie(channel, key, input),
-                 tie(velocity, note_on, note_off),
-                 [&](Number c, Number co, const MIDI::Event& i,
-                     Number& v, Trigger& on, Trigger& off)
+  sample_iterate(td, nsamples, {}, tie(channel, min, max, input),
+                 tie(key, velocity, note_on, note_off),
+                 [&](Number c, Number min, Number max, const MIDI::Event& i,
+                     Number& k, Number& v, Trigger& on, Trigger& off)
   {
-    v = last_velocity;
+    k = 0;
+    v = 0;
     on = 0;
     off = 0;
     if (i.type != MIDI::Event::Type::note_on &&
         i.type != MIDI::Event::Type::note_off)
       return;
-    if ((c < 0 || i.channel == c) && (co < 0 || i.key == co))
+    if ((c < 0 || i.channel == c) &&
+        (min < 0 || i.key >= min) &&
+        (max < 0 || i.key <= max))
     {
-      v = last_velocity = i.value / 127.0;
+      k = i.key;
+      v = i.value / 127.0;
       on = i.type == MIDI::Event::Type::note_on;
       off = i.type == MIDI::Event::Type::note_off;
     }
@@ -73,10 +78,12 @@ Dataflow::SimpleModule module
   {},
   {
     { "channel",  &KeyIn::channel },
-    { "key",      &KeyIn::key },
+    { "min",      &KeyIn::min },
+    { "max",      &KeyIn::max },
     { "input",    &KeyIn::input },
   },
   {
+    { "key",      &KeyIn::key },
     { "velocity", &KeyIn::velocity },
     { "note-on",  &KeyIn::note_on },
     { "note-off", &KeyIn::note_off },

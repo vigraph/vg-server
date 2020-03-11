c1e8662d2a909218916fe5eca686e38e796ae752
Alex Woods <alex@packetship.com>
2020-03-11 13:16:08 +0000

    Windows build with Qt window.
    - needs some tweaking still, but buildable and runnable

---
+++ desktop/desktop.h
@@ -0,0 +1,107 @@
+//==========================================================================
+// Internal definitions for dataflow desktop application
+//
+// Copyright (c) 2020 Paul Clark.  All rights reserved
+//==========================================================================
+
+#ifndef __VIGRAPH_DESKTOP_H
+#define __VIGRAPH_DESKTOP_H
+
+#include "vg-service.h"
+#include "ot-daemon.h"
+#include <QApplication>
+
+namespace ViGraph { namespace Engine {
+
+// Make our lives easier without polluting anyone else
+using namespace std;
+using namespace ObTools;
+using namespace ViGraph;
+
+//==========================================================================
+// Global state
+// Singleton instance of server-wide state
+class Server: public Daemon::Application
+{
+private:
+  Service::Server server;
+  QApplication& app;
+
+public:
+  //------------------------------------------------------------------------
+  // Constructor
+  Server(const string& licence_file, QApplication& _app):
+    server{licence_file}, app{_app} {}
+
+  //------------------------------------------------------------------------
+  // Time to sleep until next tick (microseconds)
+  int tick_wait() override { return 100; }
+
+  //------------------------------------------------------------------------
+  // Read settings from configuration
+  void read_config(const XML::Configuration& config,
+                   const string& config_filename) override
+  {
+    server.read_config(config, config_filename);
+  }
+
+  //------------------------------------------------------------------------
+  // Prerun function for child process
+  int run_priv() override
+  {
+    return server.run_priv();
+  }
+
+  //------------------------------------------------------------------------
+  // Pre main loop function
+  int pre_run() override
+  {
+    return server.pre_run();
+  }
+
+  //------------------------------------------------------------------------
+  // Main loop iteration function
+  int tick() override
+  {
+    return server.tick();
+  }
+
+  //------------------------------------------------------------------------
+  // Global configuration - called only at startup
+  // Returns whether successful
+  bool configure()
+  {
+    return server.configure();
+  }
+
+  //------------------------------------------------------------------------
+  // Global pre-configuration - called at startup
+  int preconfigure() override
+  {
+    return server.preconfigure();
+  }
+
+  //------------------------------------------------------------------------
+  // Global re-configuration - called at SIGHUP
+  void reconfigure() override
+  {
+    return server.reconfigure();
+  }
+
+  //------------------------------------------------------------------------
+  // Clean up
+  void cleanup() override
+  {
+    return server.cleanup();
+  }
+
+  //------------------------------------------------------------------------
+  // Virtual Destructor
+  ~Server() { app.closeAllWindows(); }
+};
+
+
+
+//==========================================================================
+}} //namespaces
+#endif // !__VIGRAPH_DESKTOP_H
---
+++ desktop/main.cc
@@ -0,0 +1,68 @@
+//==========================================================================
+// Main file for ViGraph engine desktop application
+//
+// Copyright (c) 2017 Paul Clark.  All rights reserved
+//==========================================================================
+
+#include "desktop.h"
+#include "vg-service.h"
+#include <QApplication>
+#include <QWebView>
+#include "ot-daemon.h"
+
+using namespace ViGraph::Engine;
+
+namespace {
+const auto server_name    = "ViGraph dataflow engine daemon";
+const auto server_version = VERSION;
+
+const auto application_url = "http://localhost:33380/";
+
+#if defined(PLATFORM_WINDOWS)
+const auto default_licence = "licence.xml";
+const auto default_config_file = "engine.cfg.xml";
+#else
+const auto default_licence = "/etc/vigraph/licence.xml";
+#ifdef DEBUG
+const auto default_config_file = "engine.cfg.xml";
+#else
+const auto default_config_file = "/etc/vigraph/engine.cfg.xml";
+#endif
+#endif
+
+const auto config_file_root = "engine";
+const auto default_log_file = "/var/log/vigraph/engine.log";
+const auto pid_file         = "/var/run/vg-engine.pid";
+}
+
+int main(int argc, char **argv)
+{
+  QApplication app{argc, argv};
+  auto t = thread{[&]()
+  {
+#if defined(PLATFORM_WINDOWS)
+    winsock_initialise();
+    wchar_t p[MAX_PATH];
+    GetModuleFileNameW(nullptr, p, MAX_PATH);
+    const auto path = File::Path(Text::UTF8::encode(&p[0]));
+    const auto licence_file = path.dirname() + "\\" + default_licence;
+    const auto config_file = path.dirname() + "\\" + default_config_file;
+    Server server(licence_file, app);
+    Daemon::Shell shell(server, server_name, server_version,
+                        config_file, config_file_root,
+                        default_log_file, pid_file);
+#else
+    Server server(default_licence, app);
+    Daemon::Shell shell(server, server_name, server_version,
+                        default_config_file, config_file_root,
+                        default_log_file, pid_file);
+#endif
+    shell.start(argc, argv);
+  }};
+  QWebView webview;
+  webview.setUrl(QUrl{application_url});
+  webview.show();
+  const int result = app.exec();
+  t.join();
+  return result;
+}
